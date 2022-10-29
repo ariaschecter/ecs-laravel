bc138c67040f098ca12a04a937bf2f4ca131862f
ariaschecter <aria.maulana.m@gmail.com>
2022-10-29 19:00:05 +0700

    [PATCH] Templating Add User

--- resources/views/users/add.blade.php
+++ resources/views/users/add.blade.php
@@ -5,37 +5,37 @@
     <div class="col-lg-6">
         <form method="POST" action="{{ url('user/add') }}">
             @csrf
-            <div class="mb-3">
+            <div>
                 <label for="user_name" class="form-label">Full Name</label>
                 <input type="text" id="user_name" name="user_name" class="form-control @error('user_name') is-invalid @enderror" value="{{ old('user_name') }}" placeholder="Full Name">
             </div>
             @error('user_name') <div class="text-danger">{{ $message }}</div> @enderror
 
-            <div class="mb-3">
+            <div class="mt-2">
                 <label for="email" class="form-label">Email</label>
                 <input type="email" id="email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" placeholder="Email">
             </div>
             @error('email') <div class="text-danger">{{ $message }}</div> @enderror
 
-            <div class="mb-3">
+            <div class="mt-2">
                 <label for="user_city" class="form-label">City</label>
                 <input type="text" id="user_city" name="user_city" class="form-control @error('user_city') is-invalid @enderror" value="{{ old('user_city') }}" placeholder="Kabupaten/Kota">
             </div>
             @error('user_city') <div class="text-danger">{{ $message }}</div> @enderror
 
-            <div class="mb-3">
+            <div class="mt-2">
                 <label for="user_age" class="form-label">Age</label>
                 <input type="number" id="user_age" name="user_age" class="form-control @error('user_age') is-invalid @enderror" value="{{ old('user_age') }}" placeholder="Umur">
             </div>
             @error('user_age') <div class="text-danger">{{ $message }}</div> @enderror
 
-            <div class="mb-3">
+            <div class="mt-2">
                 <label for="password" class="form-label">Password</label>
                 <input type="password" id="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password">
             </div>
             @error('password') <div class="text-danger">{{ $message }}</div> @enderror
 
-            <div class="mb-3">
+            <div class="mt-2">
                 <label for="password_confirmation" class="form-label">Password Confirmation</label>
                 <input type="password" id="password_confirmation" name="password_confirmation" class="form-control @error('password_confirmation') is-invalid @enderror" placeholder="Password Confirmation">
             </div>
@@ -46,7 +46,7 @@
                 <input type="text" id="example-helping" class="form-control" placeholder="Helping text">
                 <span class="help-block"><small>A block of help text that breaks onto a new line and may extend beyond one line.</small></span>
             </div> --}}
-            <div class="mb-3">
+            <div class="mt-2">
                 <label for="example-select" class="form-label">Select Role</label>
                 <select class="form-select" id="role_id" name="role_id">
                     <option value="1">Admin</option>
@@ -54,14 +54,14 @@
                 </select>
             </div>
 
-            <div class="mb-3">
+            <div class="mt-2">
                 <div class="form-check">
                     <input type="checkbox" class="form-check-input" id="active" name="active" value="1">
                     <label class="form-check-label" for="active">Check me out !</label>
                 </div>
             </div>
 
-            <button type="submit" class="btn btn-primary waves-effect waves-light">Submit</button>
+            <button type="submit" class="mt-2 btn btn-primary waves-effect waves-light">Submit</button>
         </form>
     </div> <!-- end col -->
 </div>

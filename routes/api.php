487870ed1afef8388e2f363dfbce060f65645441
nurzaman-now <imannj69@gmail.com>
2022-11-03 11:59:05 +0700

    edit routes api

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ListMapelController;
 use App\Http\Controllers\API\MapelController;
+use App\Http\Controllers\API\PaymentController;
+use App\Http\Controllers\API\PaymentMethodController;
 use App\Http\Controllers\API\RoleController;
 use App\Http\Controllers\API\SubMapelController;
 
@@ -19,22 +22,22 @@ use App\Http\Controllers\API\SubMapelController;
 */
 
 Route::controller(AuthController::class)->group(function () {
-    Route::post('register', 'register');
-    Route::post('login', 'login');
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
 
     Route::middleware('auth:sanctum')->group(function () {
-        Route::get('logout', 'logout');
-        Route::get('user/show', 'showUser');
-        Route::post('user/edit/{user:id}', 'updateUser');
+        Route::get('/logout', 'logout');
+        Route::get('/user/show', 'showUser');
+        Route::post('/user/edit/{user:id}', 'updateUser');
     });
 });
 
 Route::controller(MapelController::class)->group(function () {
-    Route::get('mapel', 'index');
-    Route::get('mapel/show/{mapel:mapel_id}', 'show');
-    Route::post('mapel/add', 'store');
-    Route::post('mapel/edit/{mapel:mapel_id}', 'update');
-    Route::delete('mapel/delete/{mapel:mapel_id}', 'destroy');
+    Route::get('/mapel', 'index');
+    Route::get('/mapel/show/{mapel:mapel_id}', 'show');
+    Route::post('/mapel/add', 'store');
+    Route::post('/mapel/edit/{mapel:mapel_id}', 'update');
+    Route::delete('/mapel/delete/{mapel:mapel_id}', 'destroy');
 });
 
 Route::controller(SubMapelController::class)->group(function () {
@@ -54,18 +57,25 @@ Route::controller(ListMapelController::class)->group(function () {
 });
 
 Route::controller(RoleController::class)->group(function () {
-    Route::get('role', 'index');
-    Route::get('role/{role:role_id}', 'show');
-    Route::post('role/add', 'store');
-    Route::post('role/edit/{role:role_id}', 'update');
-    Route::delete('role/delete/{role:role_id}', 'destroy');
+    Route::get('/role', 'index');
+    Route::get('/role/{role:role_id}', 'show');
+    Route::post('/role/add', 'store');
+    Route::post('/role/edit/{role:role_id}', 'update');
+    Route::delete('/role/delete/{role:role_id}', 'destroy');
 });
 
-// Route::controller(PaymentController::class)->group(function () {
-//     Route::get('/payment', 'index');
-//     Route::get('/payment/add', 'create');
-//     Route::post('/payment/add', 'store');
-//     Route::get('/payment/edit/{payment:payment_id}', 'edit');
-//     Route::post('/payment/edit/{payment:payment_id}', 'update');
-//     Route::get('/payment/delete/{payment:payment_id}', 'destroy');
-// });
+Route::controller(PaymentController::class)->group(function () {
+    Route::get('/payment', 'index');
+    Route::get('/payment/show', 'show');
+    Route::post('/payment/add', 'store');
+    Route::post('/payment/edit/{payment:payment_id}', 'update');
+    Route::get('/payment/delete/{payment:payment_id}', 'destroy');
+});
+
+Route::controller(PaymentMethodController::class)->group(function () {
+    Route::get('/payment_method', 'index');
+    Route::get('/payment_method/show', 'show');
+    Route::post('/payment_method/add', 'store');
+    Route::post('/payment_method/edit/{paymentMethod:payment_method_id}', 'update');
+    Route::get('/payment_method/delete/{paymentMethod:payment_method_id}', 'destroy');
+});

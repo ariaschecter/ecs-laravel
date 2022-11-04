1e6c3ede0fffbe7623bb23bd736a8f17311a3373
nurzaman-now <imannj69@gmail.com>
2022-11-04 14:42:03 +0700

    edit table access mapel

--- database/migrations/2022_10_27_154438_create_access_mapels_table.php
+++ database/migrations/2022_10_27_154438_create_access_mapels_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->id('access_mapel_id');
             $table->foreignId('id');
             $table->foreignId('mapel_id');
-            $table->foreignId('sub_mapel_id');
-            $table->foreignId('list_mapel_id');
+            $table->integer('count_sub_mapel');
+            $table->integer('count_list_mapel');
             $table->timestamps();
         });
     }

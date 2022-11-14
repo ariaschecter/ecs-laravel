844aa0fdfab8002eb8838470035c4b5c947ebc8c
ariaschecter <aria.maulana.m@gmail.com>
2022-11-14 13:55:22 +0700

    [INIT] Init Choice feature

--- app/Http/Controllers/ChoiceQuizController.php
+++ app/Http/Controllers/ChoiceQuizController.php
@@ -2,9 +2,104 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ChoiceQuiz;
+use App\Models\QuestionQuiz;
 use Illuminate\Http\Request;
 
 class ChoiceQuizController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $list_mapels = ChoiceQuiz::all();
+        return view('list_mapel.index')->with('list_mapels', $list_mapels);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $sub_mapels = QuestionQuiz::all();
+        // dd($sub_mapels);
+        return view('list_mapel.add')->with('sub_mapels', $sub_mapels);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $list_mapel = $request->validate([
+            'sub_mapel_id' => 'required',
+            'list_mapel_no' => 'required',
+            'list_mapel_name' => 'required',
+            'list_mapel_link' => 'required',
+            'list_mapel_desc' => 'required',
+        ]);
+        // dd($sub_mapel);
+
+        ChoiceQuiz::create($list_mapel);
+        $sub_mapel = QuestionQuiz::where('sub_mapel_id', $request->sub_mapel_id)->first();
+        return redirect('mapel/show/' . $sub_mapel->mapel_id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ChoiceQuiz $list_mapel)
+    {
+        $sub_mapels = QuestionQuiz::all();
+        return view('list_mapel.edit')->with([
+            'list_mapel' => $list_mapel,
+            'sub_mapels' => $sub_mapels,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ChoiceQuiz $list_mapel)
+    {
+        $update = $request->validate([
+            'sub_mapel_id' => 'required',
+            'list_mapel_no' => 'required',
+            'list_mapel_name' => 'required',
+            'list_mapel_link' => 'required',
+            'list_mapel_desc' => 'required',
+        ]);
+
+        ChoiceQuiz::where('list_mapel_id', $list_mapel->list_mapel_id)->update($update);
+        $sub_mapel = QuestionQuiz::where('sub_mapel_id', $request->sub_mapel_id)->first();
+        return redirect('mapel/show/' . $sub_mapel->mapel_id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ChoiceQuiz $list_mapel)
+    {
+        ChoiceQuiz::where('list_mapel_id', $list_mapel->list_mapel_id)->delete();
+        $sub_mapel = QuestionQuiz::where('sub_mapel_id', $list_mapel->sub_mapel_id)->first();
+        return redirect('mapel/show/' . $sub_mapel->mapel_id);
+    }
 }
---
+++ resources/views/choice/edit.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.content')
+
+@section('body')
+<div class="row">
+    <div class="col-lg-6">
+        <form method="POST" action="{{ url('list_mapel/edit/' . $list_mapel->list_mapel_id) }}">
+            @csrf
+            <div class="mt-2">
+                <label for="sub_mapel_id" class="form-label">Select Sub Mapel</label>
+                <select class="form-select" id="sub_mapel_id" name="sub_mapel_id">
+                    <option checked value="{{ $list_mapel->sub_mapel_id }}">{{ $list_mapel->sub_mapel->sub_mapel_name }}</option>
+                    @foreach ($sub_mapels as $sub_mapel)
+                        <option value="{{ $sub_mapel->sub_mapel_id }}">{{ $sub_mapel->sub_mapel_name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div>
+                <label for="list_mapel_no" class="form-label">List Mapel No</label>
+                <input type="number" id="list_mapel_no" name="list_mapel_no" class="form-control @error('list_mapel_no') is-invalid @enderror" value="{{ $list_mapel->list_mapel_no }}" placeholder="List Mapel No">
+            </div>
+            @error('list_mapel_no') <div class="text-danger">{{ $message }}</div> @enderror
+
+            <div class="mt-2">
+                <label for="list_mapel_name" class="form-label">List Mapel Name</label>
+                <input type="text" id="list_mapel_name" name="list_mapel_name" class="form-control @error('list_mapel_name') is-invalid @enderror" value="{{ $list_mapel->list_mapel_name }}" placeholder="List Mapel Name">
+            </div>
+            @error('list_mapel_name') <div class="text-danger">{{ $message }}</div> @enderror
+
+            <div class="mt-2">
+                <label for="list_mapel_link" class="form-label">List Mapel Link</label>
+                <input type="text" id="list_mapel_link" name="list_mapel_link" class="form-control @error('list_mapel_link') is-invalid @enderror" value="{{ $list_mapel->list_mapel_link }}" placeholder="List Mapel Link">
+            </div>
+            @error('list_mapel_link') <div class="text-danger">{{ $message }}</div> @enderror
+
+            <div class="mt-2">
+                <label for="list_mapel_desc" class="form-label">Text area</label>
+                <textarea class="form-control @error('list_mapel_desc') is-invalid @enderror" id="list_mapel_desc" name="list_mapel_desc" rows="10">{{ $list_mapel->list_mapel_desc }}</textarea>
+            </div>
+            @error('list_mapel_desc') <div class="text-danger">{{ $message }}</div> @enderror
+
+            <button type="submit" class="mt-2 btn btn-primary waves-effect waves-light">Submit</button>
+        </form>
+    </div> <!-- end col -->
+</div>
+@endsection
